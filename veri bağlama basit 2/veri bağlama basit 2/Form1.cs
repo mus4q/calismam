ca491fa593107ed55771157635107cb0afc8b20b
mus4q <101393637+mus4q@users.noreply.github.com>
2022-03-11 14:58:19 +0300

    Add files via upload

---
+++ veri bağlama basit 2/veri bağlama basit 2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veri_bağlama_basit_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Ogrenciler
+        {
+            public int Numara { get; set; }
+            public string AdSoyad { get; set; }
+            public string Alan { get; set; }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+    }
+}
